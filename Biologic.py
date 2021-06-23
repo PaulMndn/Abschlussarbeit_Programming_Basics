ca5c5385eb5cb53969a1ed3243188f671d4181b6
YOGA-530\Paul <paul.m.menden@gmail.com>
2021-06-24 00:31:49 +0200

    implement read_fasta_file, read_from_ncbi, transcribe, translate

--- Biologic.py
+++ Biologic.py
@@ -1,4 +1,10 @@
 import logging
+from re import A
+from Bio import SeqIO, Entrez
+import pathlib
+
+import GUI
+
 log = logging.getLogger(__name__)
 
 def get_bonds():
@@ -10,11 +16,42 @@ class biologic:
     ALPHABETS = {
         "dna": "ACTG_",
         "rna": "AUGC_",
-        "protein": "ACDEFGHIKLMNPQRSTVWY_"
+        "protein": None     # protein alphabet is added in __init__()
     }
+    # transcription
+    DNA_TO_RNA = {
+        'A': 'U',
+        'C': 'G',
+        'G': 'C',
+        'T': 'A'
+    }
+    # reverse transcription
+    RNA_TO_DNA = {v: k for k,v in DNA_TO_RNA.items()}
+
 
     def __init__(self):
-        """ Konstruktor. Falls nötig, können hier Voreinstellungen gemacht werden. """
+        """TODO: modify doc string! Konstruktor. Falls nötig, können hier Voreinstellungen gemacht werden. """
+        # create translation table
+        table_fp = GUI.SCRIPT_DIR/"data"/"genCode.txt"
+        if not table_fp.exists():
+            log.critical(f'File for codon table not found at {table_fp}')
+            raise FileNotFoundError(f'No codon table file fount at {table_fp}')
+        
+        codon_table = {}
+        with open(table_fp, "r") as ct_file:
+            for line in ct_file.readlines():
+                line.strip()
+                split = line.split()
+                aa = split[0]
+                for codon in split[1:]:
+                    codon_table[codon] = aa
+        self.codon_table = codon_table
+        log.info(f'Read codon table from {table_fp}')
+        log.debug(f'Created codon table: {codon_table}')
+
+        self.ALPHABETS['protein'] = list(codon_table.values())
+        log.debug(f'Protein alphabet added to `ALPHABETS`')
+
 
     def set_dna(self,dna):
         """ Setze die interne DNA-Sequenz. Aufgabe 1"""
@@ -44,30 +81,94 @@ class biologic:
         return self.rna
  
 
-    def read_fasta_file(self, name):
-        """" Lese eine DNA-Sequenz aus einer FASTA-Datei und speichere sie intern ab. Aufgabe 2.
-        Parameter: name: der Name der FASTA-Datei (voller Pfadname) """
-
-
-    def read_from_ncbi(self, id, mail):
-        """ Lese eine DNA-Sequenz von der NCBI-Nucleotid Datenbank. Aufgabe 2.
-        Parameter: id: Accession Number der Sequenz
-                   mail: die zu verwendende email-Adresse für den Entrez-Zugriff
-        """    
+    def read_fasta_file(self, fp: str):
+        """ Lese eine DNA-Sequenz aus einer FASTA-Datei und speichere sie intern ab. Aufgabe 2.
         
+        Argument:
+            fp: file path of fasta file
+        
+        Returns:
+            str: DNA sequence from fasta file
+        """
+        with open(fp, "r") as file:
+            # take first record from fasta file
+            record = next(SeqIO.parse(file, "fasta"), None)
+
+            if record is None:
+                # no record was read
+                log.warning(f'No record was read from fasta file at location {fp}')
+            else:
+                self.dna = str(record.seq).upper()
+                log.info(f'DNA sequence was successfully read from fasta file: {self.dna}')
+                return self.dna
+
+
+    def read_from_ncbi(self, id: str, mail: str):
+        """ Lese eine DNA-Sequenz von der NCBI-Nucleotid Datenbank. Aufgabe 2.
+        
+        Arguments:
+            id: Accession Number der Sequenz
+            mail: die zu verwendende email-Adresse für den Entrez-Zugriff
+        
+        Returns:
+            str: DNA sequence from NCBI
+        """
+        Entrez.email = mail
+        log.debug(f'{mail} set for Entrenz call.')
+        handle = Entrez.efetch(
+            db='nucleotide', 
+            id=id, 
+            rettype='fasta', 
+            retmode='text'
+        )
+        record = SeqIO.read(handle, 'fasta')
+        handle.close()
+
+        log.debug(f'Fasta file for id {id} fetched from NCBI. {record}')
+        self.dna = str(record.seq).upper()
+        log.info(f'DNA sequence for {id} fetched from NCBI.')
+        return self.dna
+
 
     def transcribe(self):  
         """ Transkribiere die intere DNA-Sequenz in RNA. Dabei sei die DNA der codogene Strang.
         Die RNA-Sequenz soll intern gespeichert und zurück gegeben werden. Aufgabe 3
-        Ergebnis: die transkribierte RNA-Sequenz (String)
+        
+        Returns:
+            str - rna sequence
         """
+        self.rna = "".join(self.DNA_TO_RNA[i] for i in self.dna)
+        log.info(f'DNA transcribed to RNA: {self.rna}')
+        return self.rna
 
-    def translate(self,initPos):    
+
+    def translate(self,initPos:int = 0):    
         """ Translatiere die intern gespeicherte RNA in eine Aminosäurensequenz, ab der 
         vorgegebenen Position. Aufgabe 4
-        Parameter: initPos: die Startposition, ab der die Translation beginnt
-        Ergebnis: die Aminosäurensequenz (String)
+
+        Argument:
+            initPos - starting position for translation
+
+        Returns:
+            :str: peptid sequence
         """
+        peptid = []
+        log.debug(f'Start translation.')
+        for i in range(initPos, len(self.rna), 3):
+            codon = self.rna[i:i+3]
+            if len(codon) < 3:
+                # No complete codon left
+                continue
+            
+            try: 
+                peptid.append(self.codon_table[codon])
+            except KeyError:
+                log.error(f'Codon {codon} not found in codon table.')
+                peptid.append(f'?({codon})')
+        
+        self.protein = "".join(peptid)
+        log.info(f'Protein sequence created. {self.protein}')
+        return self.protein
 
 
     def get_proteins(self, rf_number):
